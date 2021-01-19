4da0e8a9a142148f1325d94d5a6494f54f0c66c7
emir91 <vranacemir@gmail.com>
2021-01-19 20:35:46 +0100

    Show all user posts page finished

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const Post = require("../models/Post");
 
 exports.home = function(req,res){
     if(req.session.user){
@@ -56,3 +57,26 @@ exports.logout = function(req,res){
         res.redirect('/');    
     })
 }
+
+exports.ifUserExists = function(req, res, next){
+   User.findByUsername(req.params.username).then(function(userDocument){
+    req.profileUser = userDocument;
+    next();
+   }).catch(function(){
+       res.render('404');
+   })
+}
+
+exports.profilePostsScreen = function(req, res){
+    //ask post model for posts by a certain author id
+    Post.findByAuthorId(req.profileUser._id).then(function(posts){
+        res.render('profile', {
+            posts: posts,
+            profileUsername: req.profileUser.username,
+            profileAvatar: req.profileUser.avatar
+        });
+    }).catch(function(){
+        res.render('404')
+    })
+}
+
